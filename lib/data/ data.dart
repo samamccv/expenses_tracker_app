a51b90ee167f7316d8fa492ff316a9aee50992ea
sama mahmoud <sama44836@gmail.com>
2025-08-03 22:04:18 +0300

    Update expenses tracker app with new features and assets

--- lib/data/ data.dart
+++ lib/data/ data.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 List<Map<String, dynamic>> transactionData = [
   {
-    'icon': Icon(Icons.fastfood, color: Colors.white,),
+    'icon': Icon(FontAwesomeIcons.burger, color: Colors.white,),
     'color': Color.fromARGB(184, 131, 26, 187),
     'name': 'Food',
     'total amount': '-\$4500',
@@ -10,7 +11,7 @@ List<Map<String, dynamic>> transactionData = [
   },
 
   {
-    'icon': Icon(Icons.shopping_bag, color: Colors.white,),
+    'icon': Icon(FontAwesomeIcons.bagShopping, color: Colors.white,),
     'color': Color.fromARGB(184, 210, 24, 198),
     'name': 'Shopping',
     'total amount': '-\$2500',
@@ -18,14 +19,14 @@ List<Map<String, dynamic>> transactionData = [
   },
 
   {
-    'icon': Icon(Icons.health_and_safety, color: Colors.white,),
+    'icon': Icon(FontAwesomeIcons.kitMedical, color: Colors.white,),
     'color': Color.fromARGB(184, 26, 202, 88),
     'name': 'Health',
     'total amount': '-\$140',
     'date': 'Yesterday',
   },
   {
-    'icon': Icon(Icons.travel_explore, color: Colors.white,),
+    'icon': Icon(FontAwesomeIcons.plane, color: Colors.white,),
     'color': Color.fromARGB(248, 28, 35, 183),
     'name': 'Travel',
     'total amount': '-\$1240',
--- lib/screens/add_expenses.dart
+++ lib/screens/add_expenses.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
-class AddExpenses extends StatelessWidget {
+class AddExpenses extends StatefulWidget {
   const AddExpenses({super.key});
+
+  @override
+  State<AddExpenses> createState() => _AddExpensesState();
+}
+
+class _AddExpensesState extends State<AddExpenses> {
+  TextEditingController expenseController = TextEditingController();
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+    dateController.text = DateFormat('dd/MM/yy').format(DateTime.now());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,26 +54,27 @@ class AddExpenses extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-            
-              TextFormField(
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.dollarSign,
-                    color: Colors.blueGrey,
-                    size: 16,
+
+                  TextFormField(
+                    controller: expenseController,
+                    textAlignVertical: TextAlignVertical.center,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        FontAwesomeIcons.dollarSign,
+                        color: Colors.blueGrey,
+                        size: 16,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
+                ],
               ),
-                  ],
-              ),
-              SizedBox(height: 12,),
+              SizedBox(height: 12),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -67,27 +86,97 @@ class AddExpenses extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-              SizedBox(height: 8),
-              TextFormField(
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.list,
-                    color: Colors.blueGrey,
-                    size: 16,
+                  SizedBox(height: 8),
+                  TextFormField(
+                    readOnly: true,
+                    onTap: () {},
+                    controller: categoryController,
+                    textAlignVertical: TextAlignVertical.center,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        FontAwesomeIcons.list,
+                        color: Colors.blueGrey,
+                        size: 16,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (ctx) {
+                              bool isExpenses = false;
+                              return AlertDialog(
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextFormField(
+                                      onTap: () {
+                                        setState(() {});
+                                      },
+                                      textAlignVertical:
+                                          TextAlignVertical.center,
+                                      readOnly: true,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        prefixIcon: Icon(
+                                          FontAwesomeIcons.ellipsis,
+                                          color: Colors.blueGrey,
+                                          size: 16,
+                                        ),
+                                        suffixIcon: Icon(
+                                          FontAwesomeIcons.chevronDown,
+                                          color: Colors.blueGrey,
+                                          size: 16,
+                                        ),
+                                        hintText: 'choose your category',
+                                        hintStyle: TextStyle(
+                                          color: Colors.blueGrey,
+                                          fontSize: 14,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.vertical(
+                                            top: Radius.circular(12),
+                                          ),
+                                          borderSide: BorderSide.none,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: double.infinity,
+                                      height: 200,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.vertical(
+                                          bottom: Radius.circular(12),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        icon: Icon(
+                          FontAwesomeIcons.plus,
+                          color: Colors.blueGrey,
+                          size: 16,
+                        ),
+                      ),
+                      hintText: 'Select Category',
+                      hintStyle: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 14,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
                   ),
-                  hintText: 'Select Category',
-                  hintStyle: TextStyle(color: Colors.blueGrey,
-                  fontSize: 14),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-                  ],
+                ],
               ),
               SizedBox(height: 16),
               Column(
@@ -101,45 +190,67 @@ class AddExpenses extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-              TextFormField(
-                textAlignVertical: TextAlignVertical.center,
-                readOnly: true,
-                onTap: () {
-                  showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime.now().add(Duration(days: 365)),
-                  );
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.calendar,
-                    color: Colors.blueGrey,
-                    size: 16,
+                  TextFormField(
+                    controller: dateController,
+                    textAlignVertical: TextAlignVertical.center,
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? newDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime.now().add(Duration(days: 365)),
+                      );
+
+                      if (newDate != null) {
+                        setState(() {
+                          dateController.text = DateFormat(
+                            'dd/MM/yy',
+                          ).format(newDate);
+                          selectedDate = newDate;
+                        });
+                      }
+                    },
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(
+                        FontAwesomeIcons.calendar,
+                        color: Colors.blueGrey,
+                        size: 16,
+                      ),
+                      hintText: 'Pick Date',
+                      hintStyle: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 14,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
                   ),
-                  hintText: 'Pick Date',
-                  hintStyle: TextStyle(color: Colors.blueGrey,
-                  fontSize: 14),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-                  ],
+                ],
               ),
               SizedBox(height: 16),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Save',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
+              SizedBox(
+                width: double.infinity,
+                height: kToolbarHeight,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'Save',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
